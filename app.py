17085b93b5b4b905a3b4c2801e2acdec80dcf68a
Julio Morales <juliomorales@Julio-Morales-KLS-MacBook-Air-13.local>
2020-02-22 22:43:47 -0800

    Bitch Im a coq

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ app = Flask(__name__)             # create an app instance
 @app.route("/")                   # at the end point /
 def hello():                      # call method hello
     return render_template('index.html')         # which returns "hello world"
+
+@app.route("/about")
+def about():
+    return render_template('about-us.html')
 if __name__ == "__main__":        # on running python app.py
-    app.run()                     # run the flask app
+    app.run()                    # run the flask app
